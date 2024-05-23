904ad5580530a88bb94337a1bbac3633b1dd2b52
falpires <alariofernandopires@gmail.com>
2024-05-23 16:48:28 -0300

    initial commit

--- __main__.py
+++ __main__.py
@@ -1,53 +1,73 @@
 """An AWS Python Pulumi program"""
 
+import json
 import pulumi
 import pulumi_minio as minio
 from pulumi_kubernetes.core.v1 import Secret
 from pulumi_kubernetes.meta.v1 import ObjectMetaArgs
 
-user = minio.IamUser("minio-test-user",
-                     name="minio-test-user",
+
+config = pulumi.Config()
+
+serviceName = config.require("Name")
+serviceNamespace = config.require("Namespace")
+
+user = minio.IamUser(f"minio-iam-user",
+                     name=f"{serviceNamespace}-{serviceName}",
                      force_destroy=True)
 
-bucket = minio.S3Bucket("minio-test-bucket",
-                        bucket="minio-test-bucket")
+bucket = minio.S3Bucket(f"minio-s3-bucket",
+                        bucket=f"{serviceNamespace}-{serviceName}")
 
-iam_policy = minio.IamPolicy("minio-test-policy",
-                             name="minio-test-policy",
-                             policy="""{
-  "Version":"2012-10-17",
-  "Statement": [
-    {
-      "Sid":"ListAllBucket",
-      "Effect": "Allow",
-      "Action": [
-        "s3:ListBucket",
-        "s3:PutObject",
-        "s3:GetObject",
-        "s3:DeleteObject"
-      ],
-      "Principal":"*",
-      "Resource": [
-        "{}",
-        "{}/*"
-        ]"
-    }
-  ]
-}""".format(bucket.arn, bucket.arn))
+pulumi.export("bucket_arn",bucket.arn)
 
-iam_user_policy_attachment = minio.IamUserPolicyAttachment("minio-test-user-policy",
+def iam_user_policy(bucket_arn):
+  return pulumi.Output.all(bucket_arn).apply(
+    lambda args: json.dumps(
+      {
+        "Version":"2012-10-17",
+        "Statement": [
+          {
+            "Sid":"ListAllBucket",
+            "Effect": "Allow",
+            "Action": [
+              "s3:ListBucket",
+              "s3:PutObject",
+              "s3:GetObject",
+              "s3:DeleteObject"
+            ],
+            "Principal":"*",
+            "Resource": [
+              f"{args[0]}",
+              f"{args[0]}/*"
+              ]
+          }
+        ]
+      }
+    )
+  )
+
+
+iam_policy = minio.IamPolicy("minio-iam-policy",
+                             name=f"{serviceNamespace}-{serviceName}",
+                             policy=bucket.arn.apply(iam_user_policy)
+                            )
+
+iam_user_policy_attachment = minio.IamUserPolicyAttachment("minio-user-policy",
                                                            user_name=user.name,
                                                            policy_name=iam_policy.name)
 
 service_account = minio.IamServiceAccount("service-account",
-                                          target_user=user.name)
+                                          target_user=user.name,
+                                          policy=bucket.arn.apply(iam_user_policy))
 
 service_account_secret = Secret("service-account-secret",
                                 metadata=ObjectMetaArgs(
-                                  name="pulumi-test-secret",
+                                  name=f"{serviceNamespace}-{serviceName}-secret",
                                   namespace="default"),
                                 string_data = {
                                   "ACCESS_KEY": service_account.access_key,
                                   "SECRET_KEY": service_account.secret_key
                                 }
                                 )
+
